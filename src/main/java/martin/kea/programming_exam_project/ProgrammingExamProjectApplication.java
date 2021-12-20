b4740308ba1163fb918aa82423cad90776830766
Slimoje <mart187c@stud.kea.dk>
2021-12-20 09:43:59 +0100

    Initializing project

---
+++ src/main/java/martin/kea/programming_exam_project/ProgrammingExamProjectApplication.java
@@ -0,0 +1,13 @@
+package martin.kea.programming_exam_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingExamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingExamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/martin/kea/programming_exam_project/ProgrammingExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package martin.kea.programming_exam_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingExamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
